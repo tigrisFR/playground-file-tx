4a602d117a7c63e8837336502b9e657f18acab3b
Glitch (glitch-hello-node) <none>
2023-09-28 18:02:13 +0000

    🚤🐟 Checkpoint ./requirements.txt:51110622/63 ./run.py:51110622/72

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
+  
+    app.run(host='0.0.0.0', port=3000)
     with app.app_context():
         db.create_all()
     app.run(debug=True)
