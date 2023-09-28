35b778b5a8d369a549a1716ae9ad64ef44e98bcf
Glitch (glitch-hello-node) <none>
2023-09-28 20:01:12 +0000

    🔨😘 Checkpoint ./app/config.py:51110622/1449

--- app/config.py
+++ app/config.py
@@ -1,10 +1,14 @@
 import os
 
 class Config:
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # This should be the directory where `config.py` is.
+    PARENT_DIR = os.path.abspath(os.path.join(BASE_DIR, os.pardir))  # This gets the parent directory of `BASE_DIR`.
+    UPLOAD_FOLDER = os.path.join(PARENT_DIR, 'uploads')
+    print(f"BASE_DIR: {BASE_DIR}")
+    print(f"PARENT_DIR: {PARENT_DIR}")
+    print(f"UPLOAD_FOLDER: {UPLOAD_FOLDER}")
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "files.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(BASE_DIR), 'uploads')
     ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mp3']) # Extend as necessary
     #JWT_CSRF_CHECK_FORM = False # Disabling CSRF for simplicity. Revisit in production.
     #JWT_CSRF_IN_COOKIES = False
