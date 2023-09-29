a61eedf63bba05d4eaac46818a73d8bac16cbf2a
Adrien Nabonne <adrien@nabonne.fr>
2023-09-29 22:00:51 +0000

    Allow local export and multiple generation of invitation codes

--- generate_invite_codes.py
+++ generate_invite_codes.py
@@ -1,40 +1,65 @@
-import hmac
-import hashlib
 import os
 from p3app import create_app, db, models
+from werkzeug.security import generate_password_hash
+import click
 
-# Load the master key from the .env file
-from dotenv import load_dotenv
-load_dotenv()
+app = create_app()
+app.app_context().push()
 
-MASTER_KEY = os.environ.get('INVITATION_MASTER_KEY')
+@click.group()
+def cli():
+    """Command line interface for invitation codes management"""
+    pass
 
-def generate_invitation_code(counter):
-    master_key = MASTER_KEY.encode()
-    message = str(counter).encode()
-    
-    code = hmac.new(master_key, message, hashlib.sha256).hexdigest()
-    
-    # To ensure the code isn't excessively long, you might want to take only the first 16 characters
-    return code[:16]
+# !!! Careful !!!: CLI automatically converts '_' into '-' as it is more 
+# idiomatic in the context of command line interface
+@cli.command()
+@click.argument('count', type=click.INT)
+def generate_codes(count):
+    """Generates a specified number of new invitation codes"""
+    master_key = os.environ.get("INVITATION_MASTER_KEY")
+    if not master_key:
+        print("Please set INVITATION_MASTER_KEY in your .env file.")
+        return
 
-if __name__ == "__main__":
-    app = create_app()
-    with app.app_context():
-        counter_obj = models.InviteCounter.query.first()
-        
-        if not counter_obj:
-            counter_obj = models.InviteCounter(value=0)
-            db.session.add(counter_obj)
-        
-        counter_value = counter_obj.value
-        code = generate_invitation_code(counter_value)
+    counter_obj = models.Counter.query.first()
 
-        invite = models.InviteCode(code=code)
-        db.session.add(invite)
-
-        # Increment the counter
+    new_codes = []
+    for _ in range(count):
+        new_code = generate_password_hash(master_key + str(counter_obj.value), method='sha256')
+        new_invite = models.InviteCode(code=new_code)
+        db.session.add(new_invite)
+        new_codes.append(new_code)
         counter_obj.value += 1
-        
-        db.session.commit()
-        print(f"Generated Invitation Code: {code}")
+
+    db.session.commit()
+    print(f"{count} new codes generated:")
+    for code in new_codes:
+        print(code)
+
+# !!! Careful !!!: CLI automatically converts '_' into '-' as it is more 
+# idiomatic in the context of command line interface
+@cli.command()
+@click.argument('filename', type=click.Path(), required=False)
+@click.option('--used', is_flag=True, help="Retrieve used invitation codes instead of unused ones.")
+def export_codes(filename, used):
+    """
+    Exports used or unused invitation codes to a text file or standard output.
+
+    \b
+    FILENAME: Optional. The name of the file to which the codes will be exported.
+              If not provided, codes will be printed to the standard output.
+    """
+    codes = models.InviteCode.query.filter_by(used=used).all()
+
+    if filename:
+        with open(filename, 'w') as f:
+            for code in codes:
+                f.write(code.code + '\n')
+        print(f"Exported {len(codes)} {'used' if used else 'unused'} codes to {filename}")
+    else:
+        for code in codes:
+            print(code.code)
+
+if __name__ == '__main__':
+    cli()
