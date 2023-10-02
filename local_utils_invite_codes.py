1d57ab9bdc3cac5ed7d397e37f265c7f8736f431
Adrien Nabonne <adrien@nabonne.fr>
2023-10-02 19:26:36 +0000

    fix last invite code generation commit and add reset command

--- local_utils_invite_codes.py
+++ local_utils_invite_codes.py
@@ -1,6 +1,8 @@
 import os
 from p3app import create_app, db, models
-from werkzeug.security import generate_password_hash
+import hmac
+import hashlib
+import base64
 import click
 
 app = create_app()
@@ -10,6 +12,18 @@ app.app_context().push()
 def cli():
     """Command line interface for invitation codes management"""
     pass
+
+def generate_new_invite_code(master_key, counter):
+    """Generate a 16-character invite code based on master key and counter."""
+    # Create an HMAC hash with the master key and counter
+    message = str(counter).encode('utf-8')
+    raw_code = hmac.new(master_key.encode('utf-8'), message, hashlib.sha256).digest()
+
+    # Encode the hash in base64 to make it URL-safe and more condensed
+    b64_code = base64.urlsafe_b64encode(raw_code).decode('utf-8')
+
+    # Return the first 16 characters of the encoded hash
+    return b64_code[:16]
 
 # !!! Careful !!!: CLI automatically converts '_' into '-' as it is more 
 # idiomatic in the context of command line interface
@@ -22,11 +36,16 @@ def generate_codes(count):
         print("Please set INVITATION_MASTER_KEY in your .env file.")
         return
 
-    counter_obj = models.Counter.query.first()
+    counter_obj = models.InviteCounter.query.first()
+    if not counter_obj:
+        counter_obj = models.InviteCounter(value=0)
+        db.session.add(counter_obj)
+        db.session.commit()
+        print("Initialized the invitation code counter.")
 
     new_codes = []
     for _ in range(count):
-        new_code = generate_password_hash(master_key + str(counter_obj.value), method='sha256')
+        new_code = generate_new_invite_code(master_key, counter_obj.value)
         new_invite = models.InviteCode(code=new_code)
         db.session.add(new_invite)
         new_codes.append(new_code)
@@ -60,6 +79,24 @@ def export_codes(filename, used):
     else:
         for code in codes:
             print(code.code)
+
+# !!! Careful !!!: CLI automatically converts '_' into '-' as it is more 
+# idiomatic in the context of command line interface
+@cli.command()
+def reset_invitations():
+    """Reset invitation codes and counters."""
+    try:
+        # Delete all InviteCode records
+        models.InviteCode.query.delete()
+
+        # Delete the counter
+        models.InviteCounter.query.delete()
+        
+        # Commit the changes
+        db.session.commit()
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     cli()
